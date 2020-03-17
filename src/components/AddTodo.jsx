e2464af8dae399eced638b8a002cc03d04259477
Pergamene <smartz373@gmail.com>
2020-03-16 18:41:31 -0600

    added tags

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -2,18 +2,23 @@ import React, { useState } from 'react';
 
 const AddTodo = props => {
   const [input, setInput] = useState('');
+  const [colorInput, setColorInput] = useState('red');
   const { handleSubmit, handleClear } = props;
 
   const handleChange = event => {
     setInput(event.target.value);
   };
 
+  const handleColorChange = event => {
+    setColorInput(event.target.value);
+  }
+
   const submit = event => {
     event.preventDefault();
     const newTodo =  {
       type: 'ADD_TODO',
       item: input,
-      tag: 'red',
+      tag: colorInput,
       completed: false,
       id: Date.now(),
     };
@@ -24,6 +29,7 @@ const AddTodo = props => {
   return (
     <form onSubmit={submit} >
       <input value={input} onChange={handleChange} />
+      <input value={colorInput} onChange={handleColorChange} />
       <button type="submit">Add Todo</button>
       <button onClick={handleClear}>Clear Completed</button>
     </form>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+  },
+  completed: {
+    textDecoration: 'line-through',
+  },
+  circle: {
+    backgroundColor: props => props.tag,
+    width: '10px',
+    height: '10px',
+    borderRadius: '5px',
+    margin: '4px',
+  },
+});
 
 const Todo = props => {
-  const { item, tag } = props.data;
+  console.log(props.data.tag);
+  const classes = useStyles({tag: props.data.tag});
+  const { item, completed } = props.data;
   const { handleToggleCompleted } = props;
 
   return (
-    <div onClick={() => handleToggleCompleted()}>
-      <span>{item}</span>
-      {/* <span /> */}
+    <div className={classes.root} onClick={() => handleToggleCompleted()}>
+      {item && <div className={`${classes.circle}`}></div>}
+      <div className={completed ? classes.completed : ''}>{item}</div>
     </div>
   );
 }
